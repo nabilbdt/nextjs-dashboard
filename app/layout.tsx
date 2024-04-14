aba26d1beff67c257d06056df730dbb4e2edfa05
nabildev <nabilbayad969@gmail.com>
2024-04-14 13:30:53 +0000

    fix the error: Type error: Layout "app/layout.tsx" does not match the required types of a Next.js Layout.   "inter" is not a valid Layout export field.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-import '@/app/ui/global.css'
-import { Inter } from 'next/font/google';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
  
-export const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({
   children,
 }: {
